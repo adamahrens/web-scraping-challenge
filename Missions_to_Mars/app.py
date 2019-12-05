d7f5bc542d0e52e6fa53aaf81a85d42d136fb5ab
Adam Ahrens <adam.aahrens@gmail.com>
2019-12-04 20:57:51 -0600

    Adding template

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,41 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+import scrape_mars
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+#### Setup Flask ####
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+#mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    #destination_data = mongo.db.collection.find_one()
+
+    destination_data = {}
+    # Return template and data
+    return render_template("index.html", vacation=destination_data)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    costa_data = scrape_costa.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    # mongo.db.collection.update({}, costa_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
