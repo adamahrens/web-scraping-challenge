2e5670657504689539c99ec1bfcb2faa3a2c65dc
Adam Ahrens <adam.aahrens@gmail.com>
2020-02-11 20:58:11 -0600

    Adding slack emoji uploader

--- Missions_to_Mars/download_emojis.py
+++ Missions_to_Mars/download_emojis.py
@@ -12,22 +12,23 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/emojis")
 
-emojis = mongo.db.emojis.find()
+def download():
+    emojis = mongo.db.emojis.find()
 
-for emoji_data in emojis:
-    print(emoji_data)
-    # Open the url image, set stream to True, this will return the stream content.
-    resp = requests.get(emoji_data['url'], stream=True)
-    file_path = './emojis/' + emoji_data['file']
+    for emoji_data in emojis:
+        print(emoji_data)
+        # Open the url image, set stream to True, this will return the stream content.
+        resp = requests.get(emoji_data['url'], stream=True)
+        file_path = './emojis/' + emoji_data['file']
 
-    # Open a local file with wb ( write binary ) permission.
-    local_file = open(file_path, 'wb')
+        # Open a local file with wb ( write binary ) permission.
+        local_file = open(file_path, 'wb')
 
-    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-    resp.raw.decode_content = True
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        resp.raw.decode_content = True
 
-    # Copy the response stream raw data to local image file.
-    shutil.copyfileobj(resp.raw, local_file)
+        # Copy the response stream raw data to local image file.
+        shutil.copyfileobj(resp.raw, local_file)
 
-    # Remove the image url response object.
-    del resp
+        # Remove the image url response object.
+        del resp
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # web-scraping-challenge
 Web Scraping, MongoDB, and Flask app for U of M bootcamp
+`pip install pymongo`
+`pip install Flask-PyMongo`
+`mongo`
+`use mars`
+`show collections`
